d484fdd2de6ecf889c6425a4c4f41563a040106a
Cartrell <cartrell@gameplaycoder.com>
2025-07-09 15:33:46 -0400

    Fixed path resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     open: true,
+  },
+
+  base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
 });
